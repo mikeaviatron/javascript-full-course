76148a2604f7ad2eceff2a9b3bf96f9ece8efda5
MUKUND NIKHIL DHRUW <nikhildhruw9399@gmail.com>
2026-01-17 04:26:23 +0000

    why not to use var

---
+++ javacourse/variables.js
@@ -0,0 +1,16 @@
+const accountID =144553
+let accountEmail ="nikhil@gamil.com"
+var acccountPassword ='1234'
+accountCity="chhattisgarh"
+let accountState;
+
+// accountID=2
+accountEmail="hc@gmail.com"
+acccountPassword="225"
+accountCity="bengaluru"
+
+console.log(accountID);
+
+// prefer not to use var 
+
+console.table([acccountPassword,accountEmail,accountCity,accountState])
