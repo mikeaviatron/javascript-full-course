948728b1f9a8e333bcae375b924a3b9f356d181e
MUKUND NIKHIL DHRUW <nikhildhruw9399@gmail.com>
2026-01-17 04:25:28 +0000

    day 02

---
+++ javacourse/data.js
@@ -0,0 +1,21 @@
+"use strict"
+// this is means that use this code in new
+// alert (3+3) // wer are using nodejs, not browser
+// console.log(3+3)
+//code readability should be high
+let name="hitesh"
+let age=89
+let isLoogedIn=false
+
+// number => 2 to power 53(range)
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value hai 
+//undefined =>
+// symbol => unique
+// object 
+
+console.log(typeof null)
+//null ek object hai kehte to ek error bhi hai ise
+console.log(typeof undefined)
