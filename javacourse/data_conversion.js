a8f5e0f1f243a45de8d41d02cb43d888240f384f
MUKUND NIKHIL DHRUW <nikhildhruw9399@gmail.com>
2026-01-18 07:27:27 +0000

    day -03

---
+++ javacourse/data_conversion.js
@@ -0,0 +1,24 @@
+let score="33"
+
+// console.log(typeof(score))
+
+// let valueInNumber= Number(score)
+// console.log(typeof(valueInNumber))
+
+//"33"=>33
+//"33abc"=> Nan(not a number)
+// true=> 1; false=>0
+
+// let isLoggedIn= 1
+// let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn)
+
+// "1"=>true
+// ""=>false
+//"mukund"=>true
+
+let someNumber=33
+let StignNumber=String(someNumber)
+console.log(StignNumber)
+
+
