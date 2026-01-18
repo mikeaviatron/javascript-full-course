7b5b834ef640a1c0f38dd0d24bf6459f905324e9
MUKUND NIKHIL DHRUW <nikhildhruw9399@gmail.com>
2026-01-18 07:56:11 +0000

    problem in conversions

--- javacourse/data_conversion.js
+++ javacourse/data_conversion.js
@@ -17,8 +17,52 @@ let score="33"
 // ""=>false
 //"mukund"=>true
 
-let someNumber=33
-let StignNumber=String(someNumber)
-console.log(StignNumber)
+// let someNumber=33
+// let StignNumber=String(someNumber)
+// console.log(StignNumber)
+
+
+// ***************operation************ 
+
+
+
+let value = 5
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1= "hello"
+let str2= " mukund"
+
+let str3= str1 + str2
+// 
+
+// problems
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+"2" +2) //122
+// console.log(1+2+"2") //32 - but how in above its not added 
+
+// console.log(+true)
+// console.log(+"")
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+// gameCounter++;Postfix me pehle old value use hoti hai, fir update hoti hai.
+
+// ++gameCounter; Prefix me value pehle update hoti hai, fir expression me use hoti hai.
+
+console.log(gameCounter)
+
+
+
 
 
